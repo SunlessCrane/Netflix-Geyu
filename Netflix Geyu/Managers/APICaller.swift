f8d0873548721d55004318d93e6d8e54f9982090
SunlessCrane <geyushen20@hotmail.com>
2022-09-11 16:22:14 -0400

    finished getTrending call and response model

--- Netflix Geyu/Managers/APICaller.swift
+++ Netflix Geyu/Managers/APICaller.swift
@@ -12,10 +12,14 @@ struct Constants {
     static let baseURL  = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedToGetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
         
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) {
@@ -24,9 +28,10 @@ class APICaller {
             }
             
             do {
-                let results = JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
         }
         task.resume()
